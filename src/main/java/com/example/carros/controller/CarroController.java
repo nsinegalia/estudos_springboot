da3645a64502a034c37c7ee75ad72ea0c11ee199
nsinegalia <sinegalia.nicolas@gmail.com>
2025-04-21 00:06:11 -0300

    Removido arquivos desnecessarios

--- src/main/java/com/example/carros/controller/CarroController.java
+++ src/main/java/com/example/carros/controller/CarroController.java
@@ -1,11 +1,6 @@
 package com.example.carros.controller;
 
-import com.example.carros.geometricos.Estudo2Aula10_Heranca_Circulo;
-import com.example.carros.geometricos.Estudo2Aula10_Heranca_FiguraGeometrica;
-import com.example.carros.geometricos.Estudo2Aula10_Heranca_Quadrado;
-import com.example.carros.geometricos.Estudo2Aula10_Heranca_Triangulo;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,39 +11,14 @@ import java.util.List;
 public class CarroController {
 
     @GetMapping("helloworld")
-    public String helloWorld(){
+    public String helloWorld() {
         return "HelloWorld";
 
     }
 
     @GetMapping("celulares")
-    public List<String> novoMetodo(){
+    public List<String> novoMetodo() {
         return List.of("Iphone", "Samsung", "Motorola");
-    }
-
-    @GetMapping("geometrico/{numLados}/{tamLados}")
-    public String figuraGeometrica(@PathVariable int numLados, @PathVariable int tamLados){
-
-        Estudo2Aula10_Heranca_FiguraGeometrica figuraGeometrica;
-
-        switch (numLados){
-            case 2:
-                figuraGeometrica = new Estudo2Aula10_Heranca_Circulo(tamLados);
-                break;
-            case 3:
-                figuraGeometrica = new Estudo2Aula10_Heranca_Triangulo(tamLados, tamLados);
-                break;
-            case 4:
-                figuraGeometrica = new Estudo2Aula10_Heranca_Quadrado(tamLados);
-                break;
-
-            default: return "Figura não disponível";
-
-
-        }
-        return figuraGeometrica.getText();
-
-
     }
 
 
